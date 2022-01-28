21d50e08682ac6c1027f4951a475ee38e0370091
louisledoux <19322697+louisledoux@users.noreply.github.com>
2022-01-28 16:20:43 +0100

    feat: add AlertHeader component

--- packages/tauri-client/src/components/alert/AlertHeader.tsx
+++ packages/tauri-client/src/components/alert/AlertHeader.tsx
@@ -1,11 +1,18 @@
 function AlertHeader() {
   return (
-    <>
-      <span className="text-xs text-primary">
-        Mes alertes
+    <div className="flex flex-col">
+      <span className="text-xs text-neutralGrey text-right mb-1">
+        <button className="italic" type="button">
+          + Créer une alerte
+        </button>
       </span>
-      <hr className="border-primary" />
-    </>
+      <div className="relative">
+        <span className="text-xs text-primary absolute -top-2.5 left-5 bg-lightBackground px-2">
+          Mes alertes
+        </span>
+        <hr className="border-primary" />
+      </div>
+    </div>
   );
 }
 
