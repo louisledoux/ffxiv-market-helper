1c166420b46ae70dee18f4c1ecaea249c915a171
louisledoux <19322697+louisledoux@users.noreply.github.com>
2022-01-30 22:18:29 +0100

    fix: add universalis link to disclaimer

--- packages/tauri-client/src/components/common/Disclaimer.tsx
+++ packages/tauri-client/src/components/common/Disclaimer.tsx
@@ -1,7 +1,11 @@
 function Disclaimer() {
   return (
     <div className="w-full py-0.5 absolute bottom-0 left-0 text-small text-center bg-darkBackground">
-      <span>Market data provided by Universalis ❤</span>
+      <span>
+        Market data provided by
+        {' '}
+        <a className="text-primary" href="https://universalis.app/" target="_blank" rel="noreferrer">Universalis ❤</a>
+      </span>
     </div>
   );
 }
